aedf085a6acfe98b8f7ec7bf337c951f01af20c1
Joseph Van Boxtel <dev.josephvanboxtel@gmail.com>
2019-01-26 01:17:54 -0800

    More reorganizing. Removed unneeded code and comments. Pulled the protocol for StateController into its own file.

--- CocoaTouchAdditions/Collection.swift
+++ CocoaTouchAdditions/Collection.swift
@@ -13,3 +13,18 @@ extension Collection where Element: Equatable {
         return self.filter { other.contains($0) }
     }
 }
+
+extension Array where Element: Equatable {
+    /// Complexity: O(n)
+    public func does(_ a: Element, appearBefore b: Element) -> Bool {
+        for element in self {
+            if element == a {
+                return true
+            } else if element == b {
+                return false
+            }
+        }
+        assertionFailure("Neither element is contained.")
+        return false
+    }
+}
